c44097f798faafb7efb7a0acd60cf6792fc30c40
Ben Truyman <bentruyman@gmail.com>
2013-04-08 21:23:06 -0500

    Added basic grunt support

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    cafemocha: {
+      src: "test/**/*.js",
+      options: {
+        ui: "bdd",
+        require: ["should"]
+      }
+    },
+    jshint: {
+      src: "app/**/*.js",
+      options: {
+        jshintrc: ".jshintrc"
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-cafe-mocha");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
+  grunt.registerTask("default", ["jshint", "cafemocha"]);
+};
