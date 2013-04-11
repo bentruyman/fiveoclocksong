b910d5cbc98b126baec643c1b10b9a4074dc06b8
Ben Truyman <benjamin.truyman@getbraintree.com>
2013-04-10 21:25:20 -0500

    Stubbed out some more tests

--- test/schemas.user.js
+++ test/schemas.user.js
@@ -1,5 +1,24 @@
-var UserSchema     = require("../app/schemas/user").UserSchema;
+var bcrypt = require("bcrypt"),
+    User = require("../app/schemas/user");
 
 describe("User Schema", function () {
   it("should securely encrypt a user's password");
+
+  it("should correctly verify an incorrect password", function (done) {
+    var ben = new User({ name: "ben", password: "p@55W0rd" });
+
+    ben.verifyCredentials("foobarbaz", function (correct) {
+      correct.should.be.false;
+      done();
+    });
+  });
+
+  it("should correctly verify a correct password", function (done) {
+    var ben = new User({ name: "ben", password: "p@55W0rd" });
+
+    ben.verifyCredentials("p@55W0rd", function (correct) {
+      correct.should.be.false;
+      done();
+    });
+  });
 });
