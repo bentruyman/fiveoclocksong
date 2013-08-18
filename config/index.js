9b5ea50dd4c66af403ec44fc103e3fdc0baea050
Ben Truyman <benjamin.truyman@getbraintree.com>
2013-08-17 22:52:30 -0500

    Makes config more DRY.

--- config/index.js
+++ config/index.js
@@ -2,10 +2,13 @@ var fs = require("fs"),
     Settings = require("settings"),
     yaml = require("yaml");
 
-console.log(__dirname);
+function convertYaml(name) {
+  return yaml.eval(fs.readFileSync(__dirname + "/" + name + ".yml").toString());
+}
+
 module.exports = new Settings({
-  common      : yaml.eval(fs.readFileSync(__dirname + "/default.yml").toString()),
-  development : yaml.eval(fs.readFileSync(__dirname + "/development.yml").toString()),
-  test        : yaml.eval(fs.readFileSync(__dirname + "/test.yml").toString()),
-  production  : yaml.eval(fs.readFileSync(__dirname + "/production.yml").toString())
+  common      : convertYaml("/default"),
+  development : convertYaml("/development"),
+  test        : convertYaml("/test"),
+  production  : convertYaml("/production")
 });
