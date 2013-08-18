8039148af2a2922986cfe644cbb3e2d6cde0e27a
Ben Truyman <benjamin.truyman@getbraintree.com>
2013-08-17 22:43:37 -0500

    Converts config to YAML.

--- config/index.js
+++ config/index.js
@@ -1,8 +1,11 @@
-var Settings = require("settings");
+var fs = require("fs"),
+    Settings = require("settings"),
+    yaml = require("yaml");
 
+console.log(__dirname);
 module.exports = new Settings({
-  common:      require("./default"),
-  development: require("./development"),
-  test:        require("./test"),
-  production:  require("./production")
+  common      : yaml.eval(fs.readFileSync(__dirname + "/default.yml").toString()),
+  development : yaml.eval(fs.readFileSync(__dirname + "/development.yml").toString()),
+  test        : yaml.eval(fs.readFileSync(__dirname + "/test.yml").toString()),
+  production  : yaml.eval(fs.readFileSync(__dirname + "/production.yml").toString())
 });
